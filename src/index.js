1ff65782f9cc9b5803dc64deb64c73d9793ce274
littleacls <little.acls@gmail.com>
2026-07-06 16:41:43 -0300

    added basic JavaScript with cuurent date and time of locations

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
+function updateTime() {
+  let saoPauloElement = document.querySelector("#sao-paulo");
+  let saoPauloDateElement = saoPauloElement.querySelector(".date");
+  let saoPauloTimeElement = saoPauloElement.querySelector(".time");
+  let saoPauloTime = moment().tz("America/Sao_Paulo");
+  saoPauloDateElement.innerHTML = saoPauloTime.format("LL");
+  saoPauloTimeElement.innerHTML = saoPauloTime.format(
+    "h:mm:ss [<small>]A[</small>]",
+  );
 
+  //Chicago
+  let chicagoElement = document.querySelector("#chicago");
+  let chicagoDateElement = chicagoElement.querySelector(".date");
+  let chicagoTimeElement = chicagoElement.querySelector(".time");
+  let chicagoTime = moment().tz("America/Chicago");
+  chicagoDateElement.innerHTML = chicagoTime.format("LL");
+  chicagoTimeElement.innerHTML = chicagoTime.format(
+    "h:mm:ss [<small>]A[</small>]",
+  );
+}
+
+updateTime();
+setInterval(updateTime, 1000);
